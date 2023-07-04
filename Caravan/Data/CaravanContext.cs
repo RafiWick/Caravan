ef4d7884677bdbb87d77cbc79d193a67e92b2ed8
RafiWick <rafiwick@gmail.com>
2023-07-03 19:10:26 -0600

    Db created

---
+++ Caravan/Data/CaravanContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caravan.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Caravan.Data
+{
+    public class CaravanContext : DbContext
+    {
+        public DbSet<Passenger> Passengers { get; set; }
+        public DbSet<Wagon> Wagons { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder
+            .UseNpgsql("Host=localhost;Username=postgres;Password=password123;Database=Caravan")
+            .UseSnakeCaseNamingConvention();
+    }
+}
